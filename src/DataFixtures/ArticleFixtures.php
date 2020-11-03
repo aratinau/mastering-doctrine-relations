9eb199aa7191427bb7bad71628a89d0855257ad5
Aymeric <hello@codeparadise.xyz>
2020-11-03 08:10:26 +0100

    Owning Vs Inverse Relations

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -63,13 +63,20 @@ EOF
             $comment1->setAuthorName('Mike Ferengi');
             $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
             $comment1->setArticle($article);
+            // or:
+            // $article->addComment($comment1);
             $manager->persist($comment1);
 
             $comment2 = new Comment();
             $comment2->setAuthorName('Mike Ferengi');
             $comment2->setContent('Woohoo! I\'m going on an all-asteroid diet!');
             $comment2->setArticle($article);
+            // or:
+            //$article->addComment($comment2);
             $manager->persist($comment2);
+
+
+
         });
 
         $manager->flush();
